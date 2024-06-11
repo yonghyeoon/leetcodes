68e0eb98ca2f7a37e4d3048bce2c36415dc86140
yhlee <skwieua@gmail.com>
2024-06-11 16:41:16 +0900

    modify 2024/06/11 Problem

--- 1122. Relative Sort Array/main.cpp
+++ 1122. Relative Sort Array/main.cpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+#define MY_ANSWER
+
+
+#ifdef MY_ANSWER
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
@@ -34,6 +38,32 @@ public:
         return ans;
     }
 };
+#else
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int> ans;
+
+        for (int i = 0; i < arr2.size(); i++) {
+            for (int j = 0; j < arr1.size(); j++) {
+                if (arr2[i] == arr1[j]) {
+                    ans.push_back(arr1[j]);
+                    arr1[j] = -1;
+                }
+            }
+        }
+
+        sort(arr1.begin(), arr1.end());
+
+        for (int arr : arr1) {
+            if (arr != -1) { ans.push_back(arr); }
+        }
+
+        return ans;
+    }
+};
+
+#endif
 
 int main() {
 	Solution solution;
