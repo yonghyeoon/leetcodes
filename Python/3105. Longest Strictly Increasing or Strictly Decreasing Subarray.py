b0a539042d5ab0b2f5e4a97fe8439a781ad3d1b6
yhlee <skwieua@gmail.com>
2025-02-03 14:13:42 +0900

    add 2025/02/03 Python Solution of Problem

---
+++ Python/3105. Longest Strictly Increasing or Strictly Decreasing Subarray.py
@@ -0,0 +1,71 @@
+"""
+Problem URL : https://leetcode.com/problems/longest-strictly-increasing-or-strictly-decreasing-subarray/description
+
+You are given an array of integers nums. Return the length of the longest 
+subarray of nums which is either strictly increasing or strictly decreasing.
+
+Example 1:
+Input: nums = [1,4,3,3,2]
+Output: 2
+Explanation:
+The strictly increasing subarrays of nums are [1], [2], [3], [3], [4], and [1,4].
+The strictly decreasing subarrays of nums are [1], [2], [3], [3], [4], [3,2], and [4,3].
+Hence, we return 2.
+
+Example 2:
+Input: nums = [3,3,3,3]
+Output: 1
+Explanation:
+The strictly increasing subarrays of nums are [3], [3], [3], and [3].
+The strictly decreasing subarrays of nums are [3], [3], [3], and [3].
+Hence, we return 1.
+
+Example 3:
+Input: nums = [3,2,1]
+Output: 3
+Explanation:
+The strictly increasing subarrays of nums are [3], [2], and [1].
+The strictly decreasing subarrays of nums are [3], [2], [1], [3,2], [2,1], and [3,2,1].
+Hence, we return 3.
+
+Constraints:
+• 1 <= nums.length <= 50
+• 1 <= nums[i] <= 50
+
+"""
+
+from typing import List
+
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        maxLength = 1
+        incLength = 1
+        decLength = 1
+
+        for i in range(len(nums) - 1):
+            if nums[i+1] > nums[i]:
+                incLength += 1
+                decLength = 1
+            elif nums[i+1] < nums[i]:
+                incLength = 1
+                decLength += 1
+            else:
+                incLength = 1
+                decLength = 1
+
+            maxLength = max(maxLength, max(incLength, decLength))
+
+        return maxLength
+    
+
+# Example 1
+nums = [1,4,3,3,2]
+
+# Example 2
+# nums = [3,3,3,3]
+
+# Example 3
+# nums = [3,2,1]
+
+solution = Solution()
+print(solution.longestMonotonicSubarray(nums))
